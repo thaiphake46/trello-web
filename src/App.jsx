9f622424da0d371e3faffe377f2f1dcbe991b646
Huy Thai <huythaia123@gmail.com>
2023-12-16 15:46:55 +0700

    config mode dark light follow system

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import { useColorScheme } from '@mui/material/styles'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
 import './App.css'
+import { useColorScheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Select from '@mui/material/Select'
+import Button from '@mui/material/Button'
+import MenuItem from '@mui/material/MenuItem'
+import InputLabel from '@mui/material/InputLabel'
+import Typography from '@mui/material/Typography'
+import FormControl from '@mui/material/FormControl'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
-function ModeToggle() {
+function ModeSelect() {
   const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
   return (
-    <Button
-      variant='contained'
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+      <InputLabel id='select-dark-light-mode'>Mode</InputLabel>
+      <Select
+        labelId='select-dark-light-mode'
+        id='select-dark-light-mode'
+        value={mode}
+        label='Mode'
+        onChange={handleChange}
+      >
+        <MenuItem value={'light'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'dark'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value={'system'}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize='small' /> Light
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
@@ -21,7 +52,7 @@ function App() {
   return (
     <>
       <h1>NucMD</h1>
-      <ModeToggle />
+      <ModeSelect />
       <Typography variant='h1' gutterBottom>
         h1. Heading
       </Typography>
